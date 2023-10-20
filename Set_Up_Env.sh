bab5a1a3f180fa630d25a27c2a989567120cb4f4
Rohith Kumar <130415522+Striker0806@users.noreply.github.com>
2023-10-20 18:44:49 +0530

    Create Set_Up_Env.sh

---
+++ Set_Up_Env.sh
@@ -0,0 +1,8 @@
+chmod +x setup_JDK.sh
+chmod +x setup_android_studio.sh
+chmod +x setup_node_js.sh
+chmod +x setup_react_native.sh
+./setup_JDK.sh
+./setup_android_studio.sh
+./setup_node_js.sh
+./setup_react_native.sh
