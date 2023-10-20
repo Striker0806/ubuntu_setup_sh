0a78f3c9f9f432078490bdfa4c650ccb36ebe144
Rohith Kumar <130415522+Striker0806@users.noreply.github.com>
2023-10-20 19:18:10 +0530

    Update setup_android_studio.sh

--- setup_android_studio.sh
+++ setup_android_studio.sh
@@ -6,50 +6,35 @@ GREEN='\033[1;32m'
 BLUE='\033[1;36m'
 NC='\033[0m' # No Color
 
-# Check if ANDROID_HOME is set
-if [ -z "$ANDROID_HOME" ]; then
-    echo -e "${RED}ANDROID_HOME is not set. Exiting.${NC}"
+# Install required packages
+echo -e "${BLUE}Installing required packages${NC}"
+sudo apt update
+sudo apt install -y unzip lib32stdc++6 lib32z1
+
+# Download and Install Android Studio
+ANDROID_STUDIO_URL="https://redirector.gvt1.com/edgedl/android/studio/ide-zips/2023.2.1.9/android-studio-2023.2.1.9-linux.tar.gz"
+DOWNLOADS_DIR="$HOME/Downloads"
+echo -e "${BLUE}Downloading Android Studio${NC}"
+if wget -P $DOWNLOADS_DIR $ANDROID_STUDIO_URL; then
+    echo -e "${GREEN}Download successful!${NC}"
+else
+    echo -e "${RED}Download failed! Exiting.${NC}"
     exit 1
 fi
 
-# Update sdkmanager
-echo -e "${BLUE}Updating sdkmanager...${NC}"
-$ANDROID_HOME/tools/bin/sdkmanager --update
-
-# Check for success
-if [ $? -ne 0 ]; then
-    echo -e "${RED}Failed to update sdkmanager. Exiting.${NC}"
+echo -e "${BLUE}Installing Android Studio${NC}"
+if tar -xzf $DOWNLOADS_DIR/android-studio-ide-*-linux.tar.gz -C $HOME; then
+    echo -e "${GREEN}Installation successful!${NC}"
+else
+    echo -e "${RED}Installation failed! Exiting.${NC}"
     exit 1
 fi
 
-# Install necessary system images
-echo -e "${BLUE}Installing system images...${NC}"
-$ANDROID_HOME/tools/bin/sdkmanager "system-images;android-30;google_apis;x86_64"
-
-# Check for success
-if [ $? -ne 0 ]; then
-    echo -e "${RED}Failed to install system images. Exiting.${NC}"
-    exit 1
-fi
-
-# Set up environment variables for Android SDK
+# Set Android Studio environment variables
+echo -e "${BLUE}Setting up Android Studio environment variables${NC}"
 echo 'export ANDROID_HOME=$HOME/Android/Sdk' >> ~/.bashrc
-echo 'export PATH=$PATH:$ANDROID_HOME/emulator' >> ~/.bashrc
-echo 'export PATH=$PATH:$ANDROID_HOME/tools' >> ~/.bashrc
-echo 'export PATH=$PATH:$ANDROID_HOME/tools/bin' >> ~/.bashrc
-echo 'export PATH=$PATH:$ANDROID_HOME/platform-tools' >> ~/.bashrc
+echo 'export PATH=$PATH:$ANDROID_HOME/emulator:$ANDROID_HOME/tools:$ANDROID_HOME/tools/bin:$ANDROID_HOME/platform-tools' >> ~/.bashrc
 
-# Remember to source or restart your terminal
+# Reload .bashrc to make changes effective
 source ~/.bashrc
-
-# Create avd
-echo -e "${BLUE}Creating Android Virtual Device (AVD)...${NC}"
-echo no | $ANDROID_HOME/tools/bin/avdmanager create avd -n APP_NAME -k "system-images;android-30;google_apis;x86_64"
-
-# Check for success
-if [ $? -ne 0 ]; then
-    echo -e "${RED}Failed to create AVD. Exiting.${NC}"
-    exit 1
-fi
-
-echo -e "${GREEN}Script completed successfully!${NC}"
+echo -e "${GREEN}Setup completed!${NC}"
