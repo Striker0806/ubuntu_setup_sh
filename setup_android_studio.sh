e42e45908ce3a73a87189b9242a3e4c607ae7738
Rohith Kumar <130415522+Striker0806@users.noreply.github.com>
2023-10-20 16:08:17 +0530

    Update setup_android_studio.sh

--- setup_android_studio.sh
+++ setup_android_studio.sh
@@ -1,1 +1,28 @@
 
+#!/bin/bash
+
+# Colors for output
+RED='\033[1;31m'
+GREEN='\033[1;32m'
+BLUE='\033[1;36m'
+NC='\033[0m' # No Color
+
+# Install required packages
+echo -e "${BLUE}Installing required packages${NC}"
+sudo apt update
+sudo apt install -y unzip lib32stdc++6 lib32z1
+
+# Download and Install Android Studio
+ANDROID_STUDIO_URL="https://dl.google.com/dl/android/studio/ide-zips/3.3.0.20/android-studio-ide-182.5199772-linux.zip"
+DOWNLOADS_DIR="$HOME/Downloads"
+echo -e "${BLUE}Downloading Android Studio${NC}"
+wget -P $DOWNLOADS_DIR $ANDROID_STUDIO_URL
+
+echo -e "${BLUE}Installing Android Studio${NC}"
+unzip $DOWNLOADS_DIR/android-studio-ide-*-linux.zip -d $HOME
+
+# Set Android Studio environment variables
+echo -e "${BLUE}Setting up Android Studio environment variables${NC}"
+echo 'export ANDROID_HOME=$HOME/Android/Sdk' >> ~/.bashrc
+echo 'export PATH=$PATH:$ANDROID_HOME/emulator:$ANDROID_HOME/tools:$ANDROID_HOME/tools/bin:$ANDROID_HOME/platform-tools' >> ~/.bashrc
+source ~/.bashrc
