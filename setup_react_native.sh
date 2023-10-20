73b06f1e5f1c82811c73aee4a15017cb9cf05361
Rohith Kumar <130415522+Striker0806@users.noreply.github.com>
2023-10-20 18:37:49 +0530

    Update setup_react_native.sh

--- setup_react_native.sh
+++ setup_react_native.sh
@@ -13,26 +13,11 @@ sudo apt install -y build-essential libssl-dev git curl
 # Install watchman
 sudo apt install -y watchman
 
-
-
 # Install yarn
 sudo npm install -g yarn
 
-
 # Install detox for e2e testing
 yarn global add detox-cli
-
-# Install cocoapods (this is generally only for macOS, but if you have a need on Ubuntu you can follow specific installation steps for Linux)
-
-# Set up environment variables for Android SDK
-echo 'export ANDROID_HOME=$HOME/Android/Sdk' >> ~/.bashrc
-echo 'export PATH=$PATH:$ANDROID_HOME/emulator' >> ~/.bashrc
-echo 'export PATH=$PATH:$ANDROID_HOME/tools' >> ~/.bashrc
-echo 'export PATH=$PATH:$ANDROID_HOME/tools/bin' >> ~/.bashrc
-echo 'export PATH=$PATH:$ANDROID_HOME/platform-tools' >> ~/.bashrc
-
-# Remember to source or restart your terminal
-source ~/.bashrc
 
 # Create Android emulator (You'll need to adjust this for the exact system image you want)
 # First, update sdkmanager
@@ -41,7 +26,3 @@ $ANDROID_HOME/tools/bin/sdkmanager --update
 # Then, install necessary system images and create avd
 $ANDROID_HOME/tools/bin/sdkmanager "system-images;android-30;google_apis;x86_64"
 echo no | $ANDROID_HOME/tools/bin/avdmanager create avd -n APP_NAME -k "system-images;android-30;google_apis;x86_64"
-
-# NOTE:
-# 1. You may need to adjust Android API levels or system image strings based on what's currently available or needed.
-# 2. Some tools or utilities might need more configuration or might have been updated since this script was written. Always check official documentation when in doubt.
